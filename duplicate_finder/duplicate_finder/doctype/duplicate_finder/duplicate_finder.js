9199e31f9d2efd0eb5ed8bff4f11b57e4f324960
mats101 <matapaulchristian@gmail.com>
2018-09-07 11:11:16 +0800

    msgprint

--- duplicate_finder/duplicate_finder/doctype/duplicate_finder/duplicate_finder.js
+++ duplicate_finder/duplicate_finder/doctype/duplicate_finder/duplicate_finder.js
@@ -40,32 +40,34 @@ cur_frm.reload_doc();
             method: 'duplicate_finder.duplicate_finder.doctype.duplicate_finder.duplicate_finder.get_duplicates',
 
             callback: function (r) {
-                if(r.message.length==0){
-                                cur_frm.set_df_property("list", "read_only", cur_frm.__islocal ? 0 : 1);
+                if (r.message) {
 
-                }
-                console.log(r.message);
-                duplicates=r.message;
-                for (var key in r.message) {
-                    keys.push(key);
+                    if (r.message.length == 0) {
+                        cur_frm.set_df_property("list", "read_only", cur_frm.__islocal ? 0 : 1);
 
-                }
-                var content="";
-                                      var merge_method="cur_frm.events.merge('Customer')";
-                for (var sources = 0; sources < keys.length; sources++) {
-                    content+='<div style="width:896px;  background-color: #fafbfc;padding: 10px 15px;margin: 15px 0px;border: 1px solid #d1d8dd;border-radius: 3px;font-size: 20px;">     <p class="h6">          '+keys[sources]+'                            <a onclick=\'cur_frm.events.merge("' + keys[sources] + '")\' class="btn btn-default btn-xs pull-right" style="margin-top:-3px; margin-right: -5px;">             Merge</a>     </p>  <table style="width:100%">      '
-
-
-                    for (var duplicate = 0; duplicate < r.message[keys[sources]].length; duplicate++) {
-                        console.log(r.message[keys[sources]][duplicate]);
-                        content+=' <tr><th>'+r.message[keys[sources]][duplicate]['customer']+'</th><th>'+r.message[keys[sources]][duplicate]["email"]+'</th></tr>';
                     }
-                    content+='</table></div>';
+                    console.log(r.message);
+                    duplicates = r.message;
+                    for (var key in r.message) {
+                        keys.push(key);
+
+                    }
+                    var content = "";
+                    var merge_method = "cur_frm.events.merge('Customer')";
+                    for (var sources = 0; sources < keys.length; sources++) {
+                        content += '<div style="width:896px;  background-color: #fafbfc;padding: 10px 15px;margin: 15px 0px;border: 1px solid #d1d8dd;border-radius: 3px;font-size: 20px;">     <p class="h6">          ' + keys[sources] + '                            <a onclick=\'cur_frm.events.merge("' + keys[sources] + '")\' class="btn btn-default btn-xs pull-right" style="margin-top:-3px; margin-right: -5px;">             Merge</a>     </p>  <table style="width:100%">      '
+
+
+                        for (var duplicate = 0; duplicate < r.message[keys[sources]].length; duplicate++) {
+                            console.log(r.message[keys[sources]][duplicate]);
+                            content += ' <tr><th>' + r.message[keys[sources]][duplicate]['customer'] + '</th><th>' + r.message[keys[sources]][duplicate]["email"] + '</th></tr>';
+                        }
+                        content += '</table></div>';
+                    }
+                    $(cur_frm.fields_dict['list'].wrapper).html(content);
+
+
                 }
-                                    $(cur_frm.fields_dict['list'].wrapper).html(content);
-
-
-
             }
         });
 
@@ -85,30 +87,32 @@ cur_frm.reload_doc();
             method: 'duplicate_finder.duplicate_finder.doctype.duplicate_finder.duplicate_finder.get_duplicates',
 
             callback: function (r) {
-                console.log(r.message);
-                if(r.message.length==0){
-                                cur_frm.set_df_property("list", "read_only", cur_frm.__islocal ? 0 : 1);
+                if (r.message) {
 
-                }
-                for (var key in r.message) {
-                    keys.push(key);
+                    console.log(r.message);
+                    if (r.message.length == 0) {
+                        cur_frm.set_df_property("list", "read_only", cur_frm.__islocal ? 0 : 1);
 
-                }
-                var content="";
-                for (var sources = 0; sources < keys.length; sources++) {
-                    content+='<div style="width:896px;  background-color: #fafbfc;padding: 10px 15px;margin: 15px 0px;border: 1px solid #d1d8dd;border-radius: 3px;font-size: 20px;">     <p class="h6">          '+keys[sources]+'                            <a href="#" class="btn btn-default btn-xs pull-right" style="margin-top:-3px; margin-right: -5px;">             Merge</a>     </p>  <table style="width:100%">      '
-
-
-                    for (var duplicate = 0; duplicate < r.message[keys[sources]].length; duplicate++) {
-                        console.log(r.message[keys[sources]][duplicate]);
-                        content+=' <tr><th>'+r.message[keys[sources]][duplicate]['customer']+'</th><th>'+r.message[keys[sources]][duplicate]["email"]+'</th></tr>';
                     }
-                    content+='</table></div>';
+                    for (var key in r.message) {
+                        keys.push(key);
+
+                    }
+                    var content = "";
+                    for (var sources = 0; sources < keys.length; sources++) {
+                        content += '<div style="width:896px;  background-color: #fafbfc;padding: 10px 15px;margin: 15px 0px;border: 1px solid #d1d8dd;border-radius: 3px;font-size: 20px;">     <p class="h6">          ' + keys[sources] + '                            <a href="#" class="btn btn-default btn-xs pull-right" style="margin-top:-3px; margin-right: -5px;">             Merge</a>     </p>  <table style="width:100%">      '
+
+
+                        for (var duplicate = 0; duplicate < r.message[keys[sources]].length; duplicate++) {
+                            console.log(r.message[keys[sources]][duplicate]);
+                            content += ' <tr><th>' + r.message[keys[sources]][duplicate]['customer'] + '</th><th>' + r.message[keys[sources]][duplicate]["email"] + '</th></tr>';
+                        }
+                        content += '</table></div>';
+                    }
+                    $(cur_frm.fields_dict['list'].wrapper).html(content);
+
+
                 }
-                                    $(cur_frm.fields_dict['list'].wrapper).html(content);
-
-
-
             }
         });
 
